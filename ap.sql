32d7a71b7cfb1186a194d09c9aa6f7e6dae52992
Kashish khan <140398950+Kashishkhan26@users.noreply.github.com>
2024-07-23 01:45:29 +0800

    Add files via upload

---
+++ ap.sql
@@ -0,0 +1,11 @@
+CREATE TABLE schools (
+  id INT AUTO_INCREMENT,
+  name TEXT,
+  address TEXT,
+  city TEXT,
+  state TEXT,
+  contact NUMBER,
+  image TEXT,
+  email_id TEXT,
+  PRIMARY KEY (id)
+);
